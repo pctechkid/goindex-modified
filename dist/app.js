20788ada9fbb236fd9b09e80c94f8da1bd85040a
pctechkid <omboyronnel@gmail.com>
2021-01-09 14:48:45 +0800

    Update app.js

--- dist/app.js
+++ dist/app.js
@@ -6,4 +6,19 @@
           });
 
 		  document.write('<script src="https://cdn.jsdelivr.net/gh/pctechkid/goindex-modified@'+themeOptions.version+'/dist/js/app.da7f5f3f.js"></script>');
+		  document.write('<script src="https://cdn.jsdelivr.net/gh/pctechkid/goindex-modified@'+themeOptions.version+'/dist/js/chunk-1f1f60af.aa1f85c9"></script>');
+		  document.write('<script src="https://cdn.jsdelivr.net/gh/pctechkid/goindex-modified@'+themeOptions.version+'/dist/js/chunk-2d0af48a.2a7f864b"></script>');
+		  document.write('<script src="https://cdn.jsdelivr.net/gh/pctechkid/goindex-modified@'+themeOptions.version+'/dist/js/chunk-2d0b2ee4.4b78daef"></script>');
+		  document.write('<script src="https://cdn.jsdelivr.net/gh/pctechkid/goindex-modified@'+themeOptions.version+'/dist/js/chunk-2d0f0fbe.69f90caa"></script>');
+		  document.write('<script src="https://cdn.jsdelivr.net/gh/pctechkid/goindex-modified@'+themeOptions.version+'/dist/js/chunk-2d1e3ca3.6f34a72d"></script>');
+		  document.write('<script src="https://cdn.jsdelivr.net/gh/pctechkid/goindex-modified@'+themeOptions.version+'/dist/js/chunk-2d225416.434bb4f5"></script>');
+		  document.write('<script src="https://cdn.jsdelivr.net/gh/pctechkid/goindex-modified@'+themeOptions.version+'/dist/js/chunk-4b66a741.931e3990"></script>');
+		  document.write('<script src="https://cdn.jsdelivr.net/gh/pctechkid/goindex-modified@'+themeOptions.version+'/dist/js/chunk-6f177d7a.9e63a858"></script>');
+		  document.write('<script src="https://cdn.jsdelivr.net/gh/pctechkid/goindex-modified@'+themeOptions.version+'/dist/js/chunk-40ad1794.9718f8ab"></script>');
+		  document.write('<script src="https://cdn.jsdelivr.net/gh/pctechkid/goindex-modified@'+themeOptions.version+'/dist/js/chunk-072ae9f4.ba799a67"></script>');
+		  document.write('<script src="https://cdn.jsdelivr.net/gh/pctechkid/goindex-modified@'+themeOptions.version+'/dist/js/chunk-74a0e63a.c88af107"></script>');
+		  document.write('<script src="https://cdn.jsdelivr.net/gh/pctechkid/goindex-modified@'+themeOptions.version+'/dist/js/chunk-075e2f32.a72b9966"></script>');
+		  document.write('<script src="https://cdn.jsdelivr.net/gh/pctechkid/goindex-modified@'+themeOptions.version+'/dist/js/chunk-0609f824.2923b048"></script>');
+		  document.write('<script src="https://cdn.jsdelivr.net/gh/pctechkid/goindex-modified@'+themeOptions.version+'/dist/js/chunk-57544d36.20baab1a"></script>');
+		  document.write('<script src="https://cdn.jsdelivr.net/gh/pctechkid/goindex-modified@'+themeOptions.version+'/dist/js/chunk-02979915.dbf243db"></script>');
 		  document.write('<script src="https://cdn.jsdelivr.net/gh/pctechkid/goindex-modified@'+themeOptions.version+'/dist/js/chunk-vendors.3c185c97.js"></script>');
